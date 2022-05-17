7530a6e34e6605a58b9baebb4f444774f02aad94
Balachandranme <balachandran714@gmail.com>
2022-05-17 17:25:16 +0530

    update

--- src/org/cts/Employee.java
+++ src/org/cts/Employee.java
@@ -1,5 +1,18 @@
 package org.cts;
 
 public class Employee {
+	
+	
+	private void add() {
+		
+		System.out.println("add");
+		
+			}
+	
+	private void sub() {
+	
+			System.out.println("sub");
+
+	}
 
 }
